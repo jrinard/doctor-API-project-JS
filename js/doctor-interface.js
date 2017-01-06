ef9e045823949c04570c8e3b035e363017ef8457
Joshua Rinard <jrinard@hotmail.com>
2017-01-06 11:37:29 -0800

    adds displayResults function to append data to interface

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,12 +1,22 @@
-var Doctor = require('./../js/doctor.js').doctorModule
+var Doctor = require('./../js/doctor.js').doctorModule;
 
+var displayResults = function(results) {
+  $('#result').empty();
+  if (results.data.length > 0){
+    $('#result').append("<li id='doctor-list'></li>");
+    for (i=0; i<results.data.length; i++){
+      $('#doctor-list').append(
+        "<li>" + results.data[i].profile.first_name + " "+ results.data[i].profile.last_name + "</li>");
+    } //for end
+  } else {
+    $('#result').html("<h4>No results found for that medical issue.</h4>");
+  } // if end
+}; //function end
 
 $(document).ready(function() {
   document.getElementById("userInput").defaultValue = "toothache";
-  var currentDoctorObject = new Doctor();
+  var findDoc = new Doctor();
   $('#input-button').click(function(){
-    var medicalIssue = $('#userInput').val();
-    $('#userInput').val("");
-    currentDoctorObject.getDoctors(medicalIssue);
+    findDoc.getDoctors($('#userInput').val(), displayResults);
   });
 });
--- js/doctor.js
+++ js/doctor.js
@@ -1,14 +1,17 @@
 var apiKey = require('./../.env').apiKey;
 
 function Doctor() {
-  this.results = [];
+  var medicalIssue;
+  var searchResults;
 }
 
-Doctor.prototype.getDoctors = function(medicalIssue) {
+Doctor.prototype.getDoctors = function(medicalIssue, displayFunction) {
+  this.medicalIssue = medicalIssue;
   $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
    .then(function(result) {
-     $('.showDiv').append('<h2>' + medicalIssue + '</h2>' + "Doctors that specialize in your issue: " + result.conditions);
-      console.log(result);
+     console.log("result =" + result);
+     this.searchResults=result;
+     displayFunction(result);
     })
    .fail(function(error){
      $('.showDiv').text(error.responseJSON.message);
@@ -16,8 +19,4 @@ Doctor.prototype.getDoctors = function(medicalIssue) {
     });
 };
 
-
 exports.doctorModule = Doctor;
-
-
-// .main.humidity needs changed to point to specific field
