60bd2db6050215ac7ecabbe38327652a8131d8fa
Joshua Rinard <jrinard@hotmail.com>
2017-01-06 08:18:28 -0800

    add given code

---
+++ js/doctor.js
@@ -0,0 +1,12 @@
+var apiKey = require('./../.env').apiKey;)
+
+
+exports.getDoctors = function(medicalIssue) {
+  $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
+   .then(function(result) {
+      console.log(result);
+    })
+   .fail(function(error){
+      console.log("fail");
+    });
+};
